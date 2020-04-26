a95bde9cfa0cb0858b2bd1996ef3906bca7f9b6e
Sean Johnson <sean@maio.me>
2020-04-26 00:29:42 -0500

    Process events in a task

--- auth0_streams_elasticsearch/receiver.py
+++ auth0_streams_elasticsearch/receiver.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 
+import asyncio
 import functools
 from hmac import compare_digest
-from typing import Callable
+from typing import Callable, List
 
 import ujson
 from aiohttp import web
@@ -33,16 +34,15 @@ class ReceiverService(AIOHTTPService):
 
     async def handler(self, request: web.Request) -> web.Response:
 
-        batcher: Batcher = await self.context["batcher"]
-
         auth = request.headers.get("authorization")
         if not auth or not compare_digest(f"Bearer {self.bearer_token}", auth):
             return web.HTTPForbidden()
 
         if request.body_exists and request.can_read_body:
             try:
-                event = await request.json(loads=ujson.loads)
-                await batcher.insert(self.transform_event(event))
+                events = await request.json(loads=ujson.loads)
+                task = asyncio.create_task(self.process_events(events))
+                task.add_done_callback(self.on_process_done)
 
                 return web.json_response(
                     {"message": "Received!"},
@@ -52,6 +52,17 @@ class ReceiverService(AIOHTTPService):
                 return web.HTTPUnprocessableEntity(reason="Expected JSON body")
         else:
             return web.HTTPBadRequest()
+
+    async def process_events(self, events: List[dict]):
+        """ Takes a list of events, transforms them, and inserts them into
+            the batcher queue.
+        """
+
+        batcher: Batcher = await self.context["batcher"]
+
+        events = map(self.transform_event, events)
+        coros = map(batcher.insert, events)
+        return await asyncio.gather(coros)
 
     def transform_event(self, event: dict) -> dict:
         """ Adds the long-form description of the type identifier,
@@ -64,3 +75,9 @@ class ReceiverService(AIOHTTPService):
         ).get("event", "Unknown event type")
 
         return event
+
+    def on_process_done(self, fut: asyncio.Future):
+        """ Logs the result of the process events task.
+        """
+
+        logger.info(f"Event process task complete: {fut.result()}")
